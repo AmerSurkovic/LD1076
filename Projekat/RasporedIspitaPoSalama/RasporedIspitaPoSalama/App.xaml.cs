e9f13c270edc9acef053bbe51f68fe467c73739a
Edin Begić <ebegic2@etf.unsa.ba>
2016-05-23 15:32:47 +0200

    Dodan dodatni page

--- Projekat/RasporedIspitaPoSalama/RasporedIspitaPoSalama/App.xaml.cs
+++ Projekat/RasporedIspitaPoSalama/RasporedIspitaPoSalama/App.xaml.cs
@@ -87,7 +87,7 @@ namespace RasporedIspitaPoSalama
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(SRSPS.Views.Pocetna), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
